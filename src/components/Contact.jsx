81d52f1e6a51e3655bb6d7265bf4697a6a7acc5e
Karl <karlfredriechgetuya@gmail.com>
2023-11-23 20:35:40 +0800

    I changed the layout for contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -49,11 +49,7 @@ function Contact() {
     return (
         <>
             <div className="py-3 my-5 shadow-sm d-block d-md-flex gap-5 gap-md-3 gap-lg-5 dark">
-                <form
-                    action=""
-                    method="post"
-                    className="my-5 px-3 px-md-5 form"
-                >
+                <form action="" method="post" className="my-5 px-3 form">
                     <h1 className="fw-bold title text-md-start dark">
                         Contact
                     </h1>
@@ -113,7 +109,7 @@ function Contact() {
                         </button>
                     </div>
                 </form>
-                <div className="my-5 p-5 p-md-4 align-self-center text-center text-md-start">
+                <div className="my-5 p-5 p-md-3 align-self-center text-center text-md-start">
                     <h6>
                         Feel free to send me an {""}
                         <a
